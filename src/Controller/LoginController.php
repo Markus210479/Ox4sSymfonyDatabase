62ab0318dbba949c6beeff949768571fb7261412
Markus Latzel <m.latzel@salt-and-pepper.eu>
2023-02-09 13:53:47 +0100

    LoginFormlar

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,29 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Type\LoginType;
+use PHPUnit\Util\Xml\Validator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function login(): Response
-    {        
-        $formAction = '';
-        $formMethod = 'post';
-        $buttonContent = 'Start';
-        $formFields = [
-            ['name' => 'Email', 'input' => '', 'isValidStr' => '', 'feedbackMsg' => ''],
-            ['name' => 'Passwort', 'input' => '', 'isValidStr' => '', 'feedbackMsg' => '']
-        ];
+    private const VALID = '';
+    private const INVALID = ' is-invalid ';
 
-        return $this->render('login/login.html.twig', [
-            'formAction' => $formAction,
-            'formMethod' => $formMethod,
-            'buttonContent' => $buttonContent,
-            'formFields' => $formFields
-            ]);
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request): Response
+    {        
+        $user = new User();
+        // $user->setEmail('');
+        // $user->setPassword('');    
+
+        $form = $this->createForm(LoginType::class, $user);
+       
+        $form->handleRequest($request);
+        // if ($form->isSubmitted() && count($validator->validate($user)) == 0) {
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+
+
+        return $this->render('login/login.html.twig', ['loginForm' => $form]);
+
+        // $formAction = '/login/validate';
+        // $formMethod = 'post';
+        // $buttonContent = 'Start';
+        // $formFields = [
+        //     ['name' => 'Email', 'input' => '', 'isValidStr' => '', 'feedbackMsg' => ''],
+        //     ['name' => 'Passwort', 'input' => '', 'isValidStr' => '', 'feedbackMsg' => '']
+        // ];
+
+        // return $this->render('login/login.html.twig', [
+        //     'formAction' => $formAction,
+        //     'formMethod' => $formMethod,
+        //     'buttonContent' => $buttonContent,
+        //     'formFields' => $formFields
+        //     ]);
     }
+
+    // #[Route('/login/validate', name: 'app_validatelogin')]
+    // public function validate(ValidatorInterface $validator): Response
+    // {        
+    //     $user = new User();
+    //     $errorStr = '';
+    //     $validStr = '';
+
+    //     dd($_POST);
+    //     $user->setEmail($_POST['Email']);
+    //     $user->setPassword($_POST['Passwort']);
+    //     $errors = $validator->validate($user);
+
+    //     if(count($errors) > 0)
+    //     {
+    //         $errorStr = $errors[0]->getMessage();
+    //         $validStr = self::INVALID;
+    //     }
+
+    //     $formAction = '';
+    //     $formMethod = 'post';
+    //     $buttonContent = 'Start';
+    //     $formFields = [
+    //         ['name' => 'Email', 'input' => $user->getEmail(), 'isValidStr' => $validStr, 'feedbackMsg' => $errorStr],
+    //         ['name' => 'Passwort', 'input' => $user->getPassword(), 'isValidStr' => $validStr, 'feedbackMsg' => $errorStr]
+    //     ];
+
+    //     return $this->render('login/login.html.twig', [
+    //         'formAction' => $formAction,
+    //         'formMethod' => $formMethod,
+    //         'buttonContent' => $buttonContent,
+    //         'formFields' => $formFields
+    //         ]);
+    // }
 }
 ?>
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,9 +1,93 @@
+<?php
 namespace App\Entity;
 
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
+use Webmozart\Assert\Assert as AssertAssert;
 
 class User
 {
-    
+    public $email;
+    public $password;
+
+    public function __construct()
+    {
+        $this->email = '';
+        $this->password = '';
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    // public function getEmailHelp(): string
+    // {
+    //     return $this->emailHelp;
+    // }
+
+    // public function setEmailHelp(string $emailHelp): void
+    // {
+    //     $this->emailHelp = $emailHelp;
+    // }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+    // public $name;
+    // public $firstName;
+    // public $email;
+    // public $passWord;
+    // public $status;
+
+    // public function __construct()
+    // {
+    //     $this->name = '';
+    //     $this->firstName = '';
+    //     $this->email = '';
+    //     $this->passWord = '';
+    //     $this->status = '';
+    // }
+
+    // public array $userAttr = [
+    //     'name' => 'Name',
+    //     'firstName' => 'Vorname',
+    //     'email' => 'Email',
+    //     'passWord' => 'Passwort',
+    //     'status' => 'Status'
+    // ];
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        // $metadata->addPropertyConstraint('email', new NotBlank());
+        // $metadata->addPropertyConstraint('password', new NotBlank());
+        $metadata->addPropertyConstraint('email', new Assert\NotBlank([
+            'message' => 'Das Feld darf nicht leer sein.'
+        ]));
+        $metadata->addPropertyConstraint('email', new Assert\Email([
+            'message' => 'Die email "{{ value }}" ist keine gültige Adresse.',
+        ]));
+        $metadata->addPropertyConstraint('password', new Assert\NotBlank([
+            'message' => 'Das Feld darf nicht leer sein.'
+        ]));
+        $metadata->addPropertyConstraint('password', new Assert\Length([            
+            'min' => 8,
+            'minMessage' => 'Das Passwort muss mindestes 8 Zeichen lang sein.'
+        ]));
+
+    }
 }
+?>
